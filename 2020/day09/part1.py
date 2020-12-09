06601c5d446015dbda3bae01ba9760fdfd3fb7be
Janos Gyerik <info@janosgyerik.com>
2020-12-09 07:08:52 +0100

    Solve day 9 part 2

--- 2020/day09/part1.py
+++ 2020/day09/part1.py
@@ -29,11 +29,30 @@ def part1(preamble, nums):
         window.append(num)
 
 
+def find_range_to_sum(nums, target):
+    sum_and_pos = {0: 0}
+    cumul = 0
+    for index, num in enumerate(nums):
+        if (cumul - target) in sum_and_pos:
+            return sum_and_pos[cumul - target], index
+
+        cumul += num
+        sum_and_pos[cumul] = index + 1
+
+
+def part2(nums, target):
+    start, end = find_range_to_sum(nums, target)
+    part = nums[start:end]
+    return min(part) + max(part)
+
+
 def main():
     preamble = int(sys.argv[1])
     lines = read_lines(sys.argv[2])
     nums = [int(x) for x in lines]
-    print(part1(preamble, nums))
+    first_invalid = part1(preamble, nums)
+    print(first_invalid)
+    print(part2(nums, first_invalid))
 
 
 if __name__ == '__main__':
