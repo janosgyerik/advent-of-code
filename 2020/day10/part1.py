d82069f9ac8e9b0d296554b31546304c719550d0
Janos Gyerik <info@janosgyerik.com>
2020-12-10 22:10:00 +0100

    Solve day 10 part 2

--- 2020/day10/part1.py
+++ 2020/day10/part1.py
@@ -13,13 +13,32 @@ def part1(nums):
     seq = sorted(nums)
     seq = [0] + seq + [seq[-1] + 3]
     diffs = Counter(seq[i] - seq[i-1] for i in range(1, len(seq)))
-    print(diffs[1], diffs[3], diffs[1] * diffs[3])
+    return diffs
+
+
+def part2(nums):
+    '''
+    (0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+    1    1  1  1  2  4   4   4   8   8   8   8    8
+    ways = 0
+    '''
+    seq = sorted(nums)
+    seq = [0] + seq + [seq[-1] + 3]
+    ways = [0] * len(seq)
+    ways[0] = 1
+
+    for i, num in enumerate(seq):
+        ways[i] += sum(ways[i - d] for d in range(1, 4) if i - d >= 0 and seq[i - d] + 3 >= num)
+
+    return ways[-1]
 
 
 def main():
     lines = read_lines(sys.argv[1])
     nums = [int(x) for x in lines]
-    part1(nums)
+    diffs = part1(nums)
+    print(diffs[1] * diffs[3])
+    print(part2(nums))
 
 
 if __name__ == '__main__':
