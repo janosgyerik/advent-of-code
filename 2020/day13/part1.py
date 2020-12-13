839dfe6cbe7fd2ff9ede1384b488323a8a6c1f1d
Janos Gyerik <info@janosgyerik.com>
2020-12-13 08:15:31 +0100

    Reduce duplicated logic in day 13

--- 2020/day13/part1.py
+++ 2020/day13/part1.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-from functools import reduce
 from heapq import heappush
 
 
@@ -13,22 +12,20 @@ def read_lines(path):
 def parse_lines(lines):
     arrival = int(lines[0])
     schedule = lines[1].split(',')
-    buses = [int(x) for x in schedule if x != 'x']
-    return arrival, buses, schedule
+    buses = [(int(x), offset) for offset, x in enumerate(schedule) if x != 'x']
+    return arrival, buses
 
 
 def find_next_bus(buses, arrival):
     heap = []
-    for bus in buses:
+    for bus, _ in buses:
         div = arrival // bus + 1
         heappush(heap, (bus * div, bus))
 
     return heap[0]
 
 
-def find_contest_timestamp(schedule):
-    buses = [(int(x), offset) for offset, x in enumerate(schedule) if x != 'x']
-    # buses = sorted(buses, key=lambda x: -x[0])
+def find_contest_timestamp(buses):
     candidate = 0
     increment = 1
     for bus, offset in buses:
@@ -41,10 +38,10 @@ def find_contest_timestamp(schedule):
 
 def main():
     lines = read_lines(sys.argv[1])
-    arrival, buses, schedule = parse_lines(lines)
+    arrival, buses = parse_lines(lines)
     next_bus_time, next_bus_id = find_next_bus(buses, arrival)
     print((next_bus_time - arrival) * next_bus_id)
-    print(find_contest_timestamp(schedule))
+    print(find_contest_timestamp(buses))
 
 
 if __name__ == '__main__':
