dd12d0c31d6766abccaeebc5474f0855df66f561
Janos Gyerik <info@janosgyerik.com>
2020-12-24 07:22:01 +0100

    Solve day 24 part 2

--- 2020/day24/part1.py
+++ 2020/day24/part1.py
@@ -48,10 +48,41 @@ class Grid:
             dx, dy = dirs[p]
             x += dx
             y += dy
+
         self.grid[x, y] = 1 - self.grid[x, y]
 
     def count_black(self):
         return sum(self.grid.values())
+
+    def simulate(self):
+        min_x = min_y = max_x = max_y = 0
+        for x, y in self.grid.keys():
+            min_x = min(min_x, x)
+            max_x = max(max_x, x)
+            min_y = min(min_y, y)
+            max_y = max(max_y, y)
+        
+        copy = self.grid.copy()
+        for x in range(min_x - 1, max_x + 2):
+            for y in range(min_y - 1, max_y + 2):
+                neigh = self.count_black_neighbors(x, y)
+                if self.grid[x, y] == 1:
+                    if neigh == 0 or neigh > 2:
+                        copy[x, y] = 0
+                elif neigh == 2:
+                    copy[x, y] = 1
+
+        self.grid = copy
+
+    def count_black_neighbors(self, x, y):
+        count = 0
+        for dx, dy in dirs.values():
+            x2 = x + dx
+            y2 = y + dy
+            if self.grid[x2, y2] == 1:
+                count += 1
+
+        return count
 
 
 def main():
@@ -60,6 +91,11 @@ def main():
     grid = Grid()
     for path in paths:
         grid.flip(path)
+    print(grid.count_black())
+
+    for _ in range(100):
+        grid.simulate()
+
     print(grid.count_black())
 
 
